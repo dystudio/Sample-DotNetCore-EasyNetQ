7408fc93e396a33c009c066ee2cdf37a41520d97
Nim Anderson <danderson@blkmtn.com>
2018-12-05 15:17:55 +0000

    Initial Create

---
+++ Subscriber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using EasyNetQ;
+using EasyNetQ.NonGeneric;
+using Messages;
+
+namespace Subscriber
+{
+	internal static class Program
+	{
+		static void Main()
+		{
+			using( var bus = RabbitHutch.CreateBus( "host=localhost" ) )
+			{
+				bus.Subscribe<TextMessage>( "test", HandleTextMessage );
+
+				Console.WriteLine( "Listening for messages. Hit <return> to quit." );
+				Console.ReadLine();
+			}
+		}
+
+		static void HandleTextMessage( TextMessage textMessage )
+		{
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine( $"Got message {textMessage.Text}" );
+		}
+	}
+}

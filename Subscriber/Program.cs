2f33e44f5aeb846280406ea599c3bdc5a91cb007
david0718 <david0718@qq.com>
2019-09-04 14:12:47 +0800

    using async method to subscribe message

--- Subscriber/Program.cs
+++ Subscriber/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using EasyNetQ;
 using EasyNetQ.NonGeneric;
 using Messages;
@@ -11,9 +12,27 @@ namespace Subscriber
 		{
 			using( var bus = RabbitHutch.CreateBus( "host=localhost" ) )
 			{
-				bus.Subscribe<TextMessage>( "test", HandleTextMessage );
+				bus.SubscribeAsync<TextMessage>( "test", message => Task.Factory.StartNew(() =>
+                {
+                    // Perform some actions here
+                    // If there is a exception it will result in a task complete but task faulted which
+                    // is dealt with below in the continuation
+                }).ContinueWith(task =>
+                {
+                    if (task.IsCompleted && !task.IsFaulted)
+                    {
+                        // Everything worked out ok
+                        HandleTextMessage(message);
+                    }
+                    else
+                    {
+                        // Don't catch this, it is caught further up the hierarchy and results in being sent to the default error queue
+                        // on the broker
+                        throw new EasyNetQException("Message processing exception - look in the default error queue (broker)");
+                    }
+                }));
 
-				Console.WriteLine( "Listening for messages. Hit <return> to quit." );
+                Console.WriteLine( "Listening for messages. Hit <return> to quit." );
 				Console.ReadLine();
 			}
 		}
